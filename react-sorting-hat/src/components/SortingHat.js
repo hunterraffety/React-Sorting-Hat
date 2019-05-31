81eb41c90eb368581f81d9dea7dfcbb407eb33a2
hsrsbd <hsraffety@gmail.com>
2019-05-31 06:39:43 -0700

    quick style update to fix annoyance.

--- react-sorting-hat/src/components/SortingHat.js
+++ react-sorting-hat/src/components/SortingHat.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
+import './SortingHat.scss';
+
 class SortingHat extends React.Component {
   render() {
     return (
-      <div className='quizQuestions'>
-        <div className='house-list'>
-          <h4>{this.props.questionsFromProps.question}</h4>
-          <button className='btn' value='true' name='answer'>
-            Yes
-          </button>
-          <button className='btn' value='false' name='answer'>
-            No
-          </button>
-        </div>
+      <div className='quiz-questions'>
+        <h4>{this.props.questionsFromProps.question}</h4>
+        <button className='btn' value='true' name='answer'>
+          Yes
+        </button>
+        <button className='btn' value='false' name='answer'>
+          No
+        </button>
       </div>
     );
   }
